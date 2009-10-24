cb58d7fede6b07ebdeabb035f57f7279e6ce4679
Chuck Fouts <github@nugnug.net>
2009-10-24 16:31:19 -0400

    first commit

---
+++ dropbox-markdown.rb
@@ -0,0 +1,31 @@
+#! /usr/local/bin/ruby
+
+require 'rubygems'
+require 'rdiscount'
+
+# The program takes a Markdown document, converts it to HTML, and
+# outputs the HTML file to your Dropbox.  
+#
+# Place this file in your user's bin directory and set appropriate
+# permissions if needed.  
+#
+# The original file is renamed with the same basename and ".html" as
+# the extension.  The output file is written to the relative directory
+# '../Dropbox/notes/'. 
+#
+
+OUT_DIR = '../Dropbox/notes/'
+
+filename = ARGV.shift
+abort 'Need a filename to process!' unless filename
+
+out_filename = File.basename( filename, File.extname( filename ) ) + ".html"
+
+puts "processing: #{filename}"
+text = ""
+File.open( filename, 'r').each_line {|x| text << x } 
+
+markdown = RDiscount.new(text)
+output =  markdown.to_html
+
+File.open( OUT_DIR + out_filename, 'w' ) { |x| x.puts output }
